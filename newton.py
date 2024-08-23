2e9bfd6249fa35dbb57d993dd20686158951d8ff
Xuanlin <xuanlinamo@berkeley.edu>
2024-08-23 11:18:45 -0700

    attempt for fork

--- newton.py
+++ newton.py
@@ -5,10 +5,10 @@ def dev (f):
     fd_fuc = np.derivative(f)
     return(fd_fuc)
 
-def newtons_method(x0, function):
+def newtons_method(x0, function, eps=1e-5):
     t = 0 # iteration start point
-    xt = 0
-    while(abs(x0 - xt) > 0):
+    xt = float("inf")
+    while(abs(x0 - xt) > eps):
         xt = x0 - dev(x0)/dev(dev(x0))
         t = t + 1 # update every step (increasing)?
 
